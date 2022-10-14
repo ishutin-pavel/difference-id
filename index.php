578a6f935d9e0e6a7458284d6a79ce54bd765416
Ishutin Pavel <ishutin-pavel@mail.ru>
2022-10-14 14:53:06 +0300

    update

--- index.php
+++ index.php
@@ -3,10 +3,23 @@ require 'ids.php';
 require 'list.php';
 
 //id - list
-$result = array_diff($ids, $list);
+$result_ids_list = array_diff($ids, $list);
 
-echo '<pre>';
-print_r($result);
-echo '</pre>';
 
+echo "### В вашем списке не хватает (ids - list)<br>";
+foreach ($result_ids_list as $key => $value) {
+  echo $value . "<br>";
+}
+// echo '<pre>';
+// print_r($result_ids_list);
+// echo '</pre>';
+
+$result_list_ids = array_diff($list, $ids);
+echo "### В вашем списке лишние (list - ids)<br>";
+foreach ($result_list_ids as $key => $value) {
+  echo $value . "<br>";
+}
+// echo '<pre>';
+// print_r($result_list_ids);
+// echo '</pre>';
 ?>
